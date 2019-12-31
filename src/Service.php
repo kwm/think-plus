e27ea23ace7dba833c7d2e04da1d1747ea97fdec
Qi <i@yangqi.co>
2019-12-31 11:33:06 +0800

    使用扩展的错误处理类

--- src/Service.php
+++ src/Service.php
@@ -7,5 +7,6 @@ class Service
     public $bind = [
         'response'                      => Response::class,
         'think\fluent\event\RequestLog' => event\RequestLog::class,
+        'think\exception\Handle'        => exception\Handle::class,
     ];
 }
