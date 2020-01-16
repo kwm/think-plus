3b71c8181469e83d93d064f07baa983dab62e8e5
iiQi <i@yangqi.co>
2020-01-16 17:53:11 +0800

    优化补丁脚本

--- util/think-plus.sh
+++ util/think-plus.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-vendorPath=$(readlink -f "$(dirname "$0")/../../../")
+vendorPath=$(readlink -f "$(dirname "$0")/../")
 
 patch -N -p1 -d $vendorPath/topthink/framework < $vendorPath/kwm/think-plus/patch/think-plus.patch
 find $vendorPath/topthink/framework/ -name '*.rej' | xargs rm -f
